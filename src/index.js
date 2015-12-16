da4fcd53bcb2a5031b79c1721884677dac224559
Willy Douglas <willygdouglas@gmail.com>
2015-12-15 22:05:03 -0700

    WIP removing library dependencies from repo
    Need to fix other pages first

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 
+// Site javascript dependencies
+window.jQuery = window.$ = require('jquery');
+require('jquery.easing');
+
+
 import Home from './components/home/index';
 
 ReactDOM.render(<Home />, document.getElementById('site'));
